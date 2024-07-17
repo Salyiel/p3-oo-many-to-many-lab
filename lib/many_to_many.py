4104fd405d0c1755aaaa02c7a9dff4efc2d5474b
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-17 18:38:42 +0300

    Author class code

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,4 +1,26 @@
 class Author:
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
+
+    def __repr__(self):
+        return f'<Author name={self.name}>'
+
     pass
 
 
